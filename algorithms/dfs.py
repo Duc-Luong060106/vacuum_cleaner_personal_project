71e0c506f62a1a5ca4e658334a5da9b3e00b5e75
Phạm Trần Đức Lương <luongpham.060106@gmail.com>
2026-05-27 20:36:23 +0700

    Create dfs.py

---
+++ algorithms/dfs.py
@@ -0,0 +1,73 @@
+from utils import Node, get_solution, compare_state, gen_actions
+
+# Các hàm phụ để so sánh trạng thái đã tồn tại trong frontier và reached hay chưa
+def is_state_in_frontier(child_state, frontier):
+    for node in frontier:
+        if node.state == child_state:
+            return True
+    
+    return False
+
+def is_state_in_reached(child_state, reached):
+    return tuple(tuple(row) for row in child_state) in reached
+
+# Dfs với cách tiếp cận 1
+def depth_first_search_version_1(initial, goal_test):
+    node = Node(initial, None, None, 0)
+
+    if compare_state(node.state, goal_test):
+        return get_solution(node)
+
+    frontier = []
+    frontier.append(node)
+
+    reached = set()
+
+    while frontier:
+        node = frontier.pop()
+
+        reached.add(tuple(tuple(row) for row in node.state))
+
+        if compare_state(node.state, goal_test):
+            return get_solution(node)
+
+        for action, child_state in gen_actions(node.state):
+
+            child = Node(child_state, node, action, node.path_cost + 1)
+
+            if (not is_state_in_frontier(child.state, frontier) and 
+                not is_state_in_reached(child.state, reached)):
+                frontier.append(child)
+
+    return None
+
+# Dfs với cách tiếp cận 2
+def depth_first_search_version_2(initial, goal_test):
+    node = Node(initial, None, None, 0)
+
+    if compare_state(node.state, goal_test):
+        return get_solution(node)
+
+    frontier = []
+    frontier.append(node)
+
+    reached = set()
+
+    while frontier:
+        node = frontier.pop()
+
+        reached.add(tuple(tuple(row) for row in node.state))
+
+        for action, child_state in gen_actions(node.state):
+
+            child = Node(child_state, node, action, node.path_cost + 1)
+        
+            if (not is_state_in_frontier(child.state, frontier) and 
+                not is_state_in_reached(child.state, reached)):
+
+                if compare_state(child.state, goal_test):
+                    return get_solution(child)
+            
+                frontier.append(child)
+
+    return None
